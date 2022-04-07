768377bdfee3fc0a63e217d7011320995fa56bd2
DanielXOO <dankulinkovich@gmail.com>
2022-04-08 00:02:15 +0300

    Add swagger

---
+++ SurveyMe.WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyMe.WebApplication.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : Controller
+{
+    [HttpPost]
+    public IActionResult SignIn()
+    {
+        return Json("Sign in");
+    }
+}
--- SurveyMe.WebApplication/Program.cs
+++ SurveyMe.WebApplication/Program.cs
@@ -1,19 +1,33 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var version = builder.Configuration
+    .GetSection("ApplicationInfo:Version").Value;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(options =>
+{
+    var ApplicationVersion = builder.Configuration
+        .GetSection("ApplicationInfo:Version").Value;
+
+    options.SwaggerDoc($"api-v{ApplicationVersion}", new OpenApiInfo
+    {
+        Title = "SurveyMe Api",
+        Version = ApplicationVersion
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint($"/swagger/api-v{version}/swagger.json", $"api-v{version}");
+    });
 }
 
 app.UseHttpsRedirection();
