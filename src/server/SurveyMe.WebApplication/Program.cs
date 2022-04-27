8ea4b769c6c75f95fea6337fd3f2988f8ae8ab0c
DanielXOO <dankulinkovich@gmail.com>
2022-04-27 13:18:19 +0300

    Move to server folder

--- src/server/SurveyMe.WebApplication/Program.cs
+++ src/server/SurveyMe.WebApplication/Program.cs
@@ -1,20 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using SurveyMe.Common.Microsoft.Logging;
+using SurveyMe.Repositories;
+using SurveyMe.WebApplication;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.ConfigureLogging(logBuilder =>
+{
+    logBuilder.AddLogger();
+    logBuilder.AddFile(builder.Configuration.GetSection("Serilog:FileLogging"));
+});
+
+var version = builder.Configuration
+    .GetSection("ApplicationInfo:Version").Value;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(options =>
+{
+    var applicationVersion = builder.Configuration
+        .GetSection("ApplicationInfo:Version").Value;
+
+    options.SwaggerDoc($"api-v{applicationVersion}", new OpenApiInfo
+    {
+        Title = "SurveyMe Api",
+        Version = applicationVersion
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SurveyMeDbContext>(options 
+    => options.UseSqlServer(builder.Configuration
+        .GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint($"/swagger/api-v{version}/swagger.json", $"api-v{version}");
+    });
 }
+
+app.Services.CreateDbIfNotExists();
 
 app.UseHttpsRedirection();
 
