e5c2af9ac7678bcf321d1ec9c4525c9f16b1309a
DanielXOO <dankulinkovich@gmail.com>
2022-04-28 12:17:39 +0300

    Add account basic actions

--- src/server/SurveyMe.WebApplication/Controllers/AccountController.cs
+++ src/server/SurveyMe.WebApplication/Controllers/AccountController.cs
@@ -1,13 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SurveyMe.DomainModels;
+using SurveyMe.Foundation.Services.Abstracts;
+using SurveyMe.WebApplication.Models.RequestModels;
 
 namespace SurveyMe.WebApplication.Controllers;
 
 [ApiController]
-public class AccountController : Controller
+[Route("[controller]")]
+public sealed class AccountController : Controller
 {
-    [HttpPost("SignIn")]
-    public IActionResult SignIn()
+    private readonly ILogger<AccountController> _logger;
+    private readonly IAccountService _accountService;
+    
+    
+    public AccountController(ILogger<AccountController> logger, IAccountService accountService)
     {
-        return Json("Sign in");
+        _accountService = accountService;
+        _logger = logger;
+    }
+    
+    
+    [HttpPost(nameof(Login))]
+    public async Task<IActionResult> Login(UserLoginRequest user)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _accountService.SignInAsync(user.Login, user.Password);
+
+        if (!result.IsSuccessful)
+        {
+            foreach (var error in result.ErrorMessages)
+            {
+                ModelState.AddModelError(string.Empty, error);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        return Ok();
+    }
+    
+    [HttpPost(nameof(Registration))]
+    public async Task<IActionResult> Registration(UserRegistrationRequest userModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = new User(){UserName = userModel.Login, DisplayName = userModel.Name};
+
+        var result = await _accountService.RegisterAsync(user, userModel.Password);
+
+        if (!result.IsSuccessful)
+        {
+            foreach (var error in result.ErrorMessages)
+            {
+                ModelState.AddModelError(string.Empty, error);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        return Ok();
+    }
+    
+    [HttpPost(nameof(LogOut))]
+    public async Task<IActionResult> LogOut()
+    {
+        await _accountService.SignOutAsync();
+            
+        return Ok(); 
     }
 }
---
+++ src/server/SurveyMe.WebApplication/Models/RequestModels/UserLoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurveyMe.WebApplication.Models.RequestModels;
+
+public sealed class UserLoginRequest
+{
+    [Required(ErrorMessage = "Login cannot be empty")]
+    public string Login { get; set; }
+
+    [Required(ErrorMessage = "Password cannot be empty")]
+    public string Password { get; set; }
+}
